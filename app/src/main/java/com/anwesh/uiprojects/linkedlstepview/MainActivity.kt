21ec057943b61a9025f38bb72119a19bb3ef8051
Anwesh43 <anweshthecool0@gmail.com>
2018-10-21 13:39:22 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedlstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedlstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.lstepview.LStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
