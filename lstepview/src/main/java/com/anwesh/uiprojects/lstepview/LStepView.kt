3d5e152c1f83a870f56795b3d38906e1505dc5d3
Anwesh43 <anweshthecool0@gmail.com>
2018-10-21 11:55:50 +0530

    adding lstepview module

--- lstepview/src/main/java/com/anwesh/uiprojects/lstepview/LStepView.kt
+++ lstepview/src/main/java/com/anwesh/uiprojects/lstepview/LStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.lstepview
 /**
  * Created by anweshmishra on 21/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
