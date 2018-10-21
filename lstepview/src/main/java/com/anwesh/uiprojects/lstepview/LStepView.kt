62064b023a35f8322357598cfa361587b39e71f5
Anwesh43 <anweshthecool0@gmail.com>
2018-10-21 12:10:07 +0530

    adding LStepView class

--- lstepview/src/main/java/com/anwesh/uiprojects/lstepview/LStepView.kt
+++ lstepview/src/main/java/com/anwesh/uiprojects/lstepview/LStepView.kt
@@ -32,3 +32,21 @@ fun Canvas.drawLStep(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class LStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
